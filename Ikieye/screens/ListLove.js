ad5e4194e1eca13dc0ec3e94254d685623b4d470
Jonathan Kaneshiro <jkaneshiro97@gmail.com>
2020-09-09 22:14:03 -0700

    import hooks for all list screens

--- Ikieye/screens/ListLove.js
+++ Ikieye/screens/ListLove.js
@@ -8,34 +8,12 @@ import { generate } from "shortid";
 import ListItem from "../components/ListItem";
 import EntryForm from '../components/EntryForm';
 
+import { useItems } from "../hooks";
+
 // Store list items in a database
 // Give tips on what they can enter
 
-const useItems = () => {
-    const [items, setItems] = useState([]);
-    const addItem = text => {
-        if (!text) {
-            Alert.alert(
-                'No item entered',
-                'Please enter an item when adding to your list',
-                [
-                    {
-                        text: 'Understood',
-                        style: 'cancel',
-                    },
-                ],
-                { cancelable: true },
-            );
-        } else {
-            const newItem = { id: generate(), text };
-            setItems([newItem, ...items]);
-        }
-    };
-    return { items, addItem };
-};
-
 export default function ListLove({ name }) {
-
     const { items, addItem } = useItems();
     return (
         <ScreenContainer style={styles.container}>
