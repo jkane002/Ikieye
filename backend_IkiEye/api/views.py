9bb46a76c6de4f2c5a0ae14710399f23a7859460
Jonathan Kaneshiro <jkaneshiro97@gmail.com>
2020-11-19 22:10:07 -0800

    Provided API Overview

--- backend_IkiEye/api/views.py
+++ backend_IkiEye/api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+# Option is GET 
+@api_view(['GET'])
+def apiOverview(request):
+    """Overview of the API"""
+    api_urls = {
+        'List':'/task-list/',
+        'Detail View':'/task-detail/<str:pk>/',
+        'Create':'/task-create/',
+        'Update':'/task-update/<str:pk>/',
+        'Delete':'/task-delete/<str:pk>/',
+    }
+    # To pass any other JSON-serializable object you must set the safe parameter to False
+    return Response(api_urls)
